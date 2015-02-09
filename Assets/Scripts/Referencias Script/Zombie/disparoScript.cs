099e8a84dfcefffdae3df32283cb1cda2762f9b9
javiblanco <javiblancotorres@gmail.com>
2015-02-09 17:49:37 +0100

    PrefabMario + Scripts referencia

---
+++ Assets/Scripts/Referencias Script/Zombie/disparoScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class disparoScript : MonoBehaviour {
+
+	public GameObject bala;
+	public Transform puntoDisparo;
+
+
+	public void Fire (){
+	    
+		//Si el jugador tiene bala distinta a  null (ninguna) generara un disparo desde el puntoDisparo y con rotacion estandar, y si el jugador no tiene bala, no pasara nada.
+		if (bala != null) {
+			var clone = Instantiate (bala, puntoDisparo.position,
+			                         Quaternion.identity) as GameObject;
+			clone.transform.localScale = transform.localScale;
+		}
+	}
+}
---
+++ Assets/Scripts/saltoScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class saltoScript : MonoBehaviour {
+
+	public float jumpSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if( (Input.GetKeyDown("up"))
+			rigidbody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+	}
+	}
+
