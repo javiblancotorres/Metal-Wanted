148bf2795911ec3c4b179042948e00c4e7a72620
javiblanco <javiblancotorres@gmail.com>
2015-02-12 18:38:32 +0100

    conseguir vida #302

--- Assets/Scripts/saludScript.cs
+++ Assets/Scripts/saludScript.cs
@@ -5,13 +5,27 @@ public class saludScript : MonoBehaviour {
 
 	public int salud = 100;
 
+	private int maxsalud;
+
 	// Use this for initialization
 	void Start () {
-	
+		maxsalud = salud;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (salud > maxsalud) {
+			salud = maxsalud ;
+		}
 	}
+	void OnCollisionEnter2D (Collision2D target){
+
+		if (target.transform.tag == "salud") {
+			salud = salud + 5;	
+
+
+		} 
+	}
+
 }
