bf5486a7de9ef57761bf4a6d72bbc35c03d49788
javiblanco <javiblancotorres@gmail.com>
2015-02-12 18:18:16 +0100

    Creacion sistema de salud

---
+++ Assets/Scripts/saludScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class saludScript : MonoBehaviour {
+
+	public int salud = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
