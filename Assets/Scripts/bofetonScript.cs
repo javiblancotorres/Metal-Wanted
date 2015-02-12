303cbc9cd0f3b4e91c692e3cad0cdf661abf8de6
javiblanco <javiblancotorres@gmail.com>
2015-02-12 19:15:10 +0100

    Protagonista:Golpear#288 (Sin terminar)

---
+++ Assets/Scripts/bofetonScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bofetonScript : MonoBehaviour {
+
+	public bool bofeton;
+
+	private Animator animacion; 
+
+
+
+	// Use this for initialization
+	void Start () {
+		animacion = GetComponent<Animator> ();
+
+		bofeton = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey ("z")) {
+			animacion.SetBool ("bofeton", true);		
+		}else{
+			animacion.SetBool ("bofeton", false);
+
+	}
+}
+
+}
